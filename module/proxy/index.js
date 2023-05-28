7f041fd5347f07a48b0f18d0381f197029050cde
jaime <garciadiazjaime@gmail.com>
2023-05-28 14:56:09 -0500

    feat: fetch https

--- module/proxy/index.js
+++ module/proxy/index.js
@@ -46,4 +46,43 @@ router.get("/", async (req, res) => {
   res.send(html);
 });
 
+router.get("/instagram", async (req, res) => {
+  const url =
+    "https://www.instagram.com/api/v1/tags/logged_out_web_info/?tag_name=valledeguadalupe";
+  const headers = {
+    referer: "https://www.instagram.com/explore/tags/valledeguadalupe/",
+    "user-agent":
+      "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36",
+    "x-ig-app-id": "936619743392459",
+  };
+
+  const cert = base64.decode(process.env.MINT_CERT);
+  const options = {
+    cert,
+    key: cert,
+  };
+
+  const sslConfiguredAgent = new https.Agent(options);
+
+  let response;
+
+  try {
+    response = await fetch(url, {
+      headers,
+      agent: sslConfiguredAgent,
+      method: "get",
+    });
+  } catch (error) {
+    console.log(error);
+  }
+
+  if (!response) {
+    res.sendStatus(400);
+  }
+
+  const body = await response.json();
+
+  res.send(body);
+});
+
 module.exports = router;
